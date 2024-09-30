570b9950871cfbb4783b8433af2a22a96c45956e
Kelsey050218 <159514590+Kelsey050218@users.noreply.github.com>
2024-09-30 16:39:25 +0800

    生成sql语句

---
+++ tasty.sql
@@ -0,0 +1,401 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for macos14 (arm64)
+--
+-- Host: 127.0.0.1    Database: tasty
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `collect`
+--
+
+DROP TABLE IF EXISTS `collect`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `collect` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '收藏id',
+  `recipe_id` bigint DEFAULT NULL COMMENT '食谱id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='收藏';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `collect`
+--
+
+LOCK TABLES `collect` WRITE;
+/*!40000 ALTER TABLE `collect` DISABLE KEYS */;
+/*!40000 ALTER TABLE `collect` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `note_id` bigint DEFAULT NULL COMMENT '笔记id',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '评论id',
+  `user_id` bigint DEFAULT NULL COMMENT '评论的人id',
+  `comment_time` date DEFAULT NULL COMMENT '评论时间',
+  `remark` varchar(255) NOT NULL COMMENT '评论',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='评论';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `guide`
+--
+
+DROP TABLE IF EXISTS `guide`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `guide` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '指南id',
+  `recipe_id` bigint DEFAULT NULL COMMENT '食谱id',
+  `steps` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `guide`
+--
+
+LOCK TABLES `guide` WRITE;
+/*!40000 ALTER TABLE `guide` DISABLE KEYS */;
+INSERT INTO `guide` VALUES (1,1,'[买回来的青菜要先用水泡30分钟后在一根一根的用手来揉洗将污垢含存量洗掉。,洗好的青菜要滤水放入小菜盆中，这样的青菜含水就少了，做出的汤菜就新鲜。,用炒锅来做青菜汤要景炒锅刷干净没有锈斑，做出的汤菜就是原色的。,将备好的豆油放入炒锅中加热后，将备好的青菜放入热锅中炒软放入适量的水烧开后放入备好的精盐。,青菜汤汁非常新鲜，煮好的青菜还是绿色的，青菜汤不咸适合于米饭配着吃。]\n \n');
+/*!40000 ALTER TABLE `guide` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredient` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '成分id',
+  `recipe_id` bigint DEFAULT NULL COMMENT '食谱id',
+  `picture` varchar(255) NOT NULL COMMENT '成分图片',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='成分料表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient`
+--
+
+LOCK TABLES `ingredient` WRITE;
+/*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `like`
+--
+
+DROP TABLE IF EXISTS `like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `like` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '点赞id',
+  `noteuser_id` bigint DEFAULT NULL COMMENT '笔记拥有者id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='赞过';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `like`
+--
+
+LOCK TABLES `like` WRITE;
+/*!40000 ALTER TABLE `like` DISABLE KEYS */;
+/*!40000 ALTER TABLE `like` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '菜单ID',
+  `menu_name` varchar(50) NOT NULL COMMENT '菜单名称',
+  `perms` varchar(100) DEFAULT NULL COMMENT '权限标识',
+  `create_by` bigint DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint DEFAULT NULL COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2034 DEFAULT CHARSET=utf8mb3 COMMENT='菜单权限表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `motivation`
+--
+
+DROP TABLE IF EXISTS `motivation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `motivation` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `sentence` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='每日鸡汤';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `motivation`
+--
+
+LOCK TABLES `motivation` WRITE;
+/*!40000 ALTER TABLE `motivation` DISABLE KEYS */;
+INSERT INTO `motivation` VALUES (1,'Believe in yourself, and all things are possible.'),(2,'The only way to do great work is to love what you do.'),(3,'The only limit to our realization of tomorrow will be our doubts of today.'),(4,'The best way to predict the future is to create it.'),(5,'Impossible is just an opinion.'),(6,'The future belongs to those who believe in the beauty of their dreams.'),(7,'Embrace the journey, not just the destination. Enjoy the process.'),(8,'Be the change you wish to see in the world. '),(9,'Don\'t wait for the perfect moment - take the moment and make it perfect.'),(10,'Your success is determined by your own mindset and the actions you take each day.');
+/*!40000 ALTER TABLE `motivation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `note`
+--
+
+DROP TABLE IF EXISTS `note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `note` (
+  `note_id` bigint NOT NULL AUTO_INCREMENT COMMENT '笔记id',
+  `noteuser_id` bigint DEFAULT NULL COMMENT '笔记拥有者id',
+  `note_picture` varchar(255) DEFAULT NULL COMMENT '笔记照片',
+  `describe` varchar(255) DEFAULT NULL COMMENT '笔记描述',
+  `recipe_id` bigint DEFAULT NULL COMMENT '食谱id',
+  `note_time` timestamp NULL DEFAULT NULL COMMENT '笔记发布时间',
+  `status` int NOT NULL DEFAULT '1' COMMENT '笔记状态',
+  PRIMARY KEY (`note_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='笔记';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note`
+--
+
+LOCK TABLES `note` WRITE;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipe`
+--
+
+DROP TABLE IF EXISTS `recipe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recipe` (
+  `recipe_id` bigint NOT NULL AUTO_INCREMENT COMMENT '食谱id',
+  `menu` varchar(10) DEFAULT NULL COMMENT '菜谱',
+  `type` varchar(10) DEFAULT NULL COMMENT '类型',
+  `people` int DEFAULT NULL COMMENT '人数',
+  `time` int DEFAULT NULL COMMENT '准备时间',
+  `calories` int DEFAULT NULL COMMENT '热量',
+  `recipe_picture` varchar(255) DEFAULT NULL COMMENT '食谱照片',
+  `score` int DEFAULT NULL COMMENT '星级评分',
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`recipe_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='食谱';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipe`
+--
+
+LOCK TABLES `recipe` WRITE;
+/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
+INSERT INTO `recipe` VALUES (1,'breakfast','soup',2,600,100,'https://typora050218.oss-cn-hangzhou.aliyuncs.com/pic/%E8%8D%89%E6%9C%AC%E6%B1%A4%E4%B8%8B%E8%BD%BD.jpeg',5,'草本汤');
+/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `report` (
+  `report_id` bigint NOT NULL AUTO_INCREMENT,
+  `report_note_id` bigint NOT NULL,
+  `report_user_id` bigint NOT NULL,
+  `report_context` varchar(255) DEFAULT NULL,
+  `report_status` int NOT NULL,
+  `creat_time` datetime NOT NULL,
+  `handle_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`report_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report`
+--
+
+LOCK TABLES `report` WRITE;
+/*!40000 ALTER TABLE `report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `role_name` varchar(30) NOT NULL COMMENT '角色名称',
+  `role_key` varchar(100) NOT NULL COMMENT '角色权限字符串',
+  `create_by` bigint DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint DEFAULT NULL COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3 COMMENT='角色信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'用户','user',NULL,NULL,NULL,NULL),(2,'管理员','admin',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_menu`
+--
+
+DROP TABLE IF EXISTS `role_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_menu` (
+  `role_id` bigint NOT NULL COMMENT '角色id',
+  `menu_id` bigint NOT NULL COMMENT '权限id',
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='角色权限关联表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_menu`
+--
+
+LOCK TABLES `role_menu` WRITE;
+/*!40000 ALTER TABLE `role_menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `user_name` varchar(10) NOT NULL COMMENT '用户名',
+  `password` varchar(20) NOT NULL COMMENT '用户密码',
+  `phone` char(11) NOT NULL COMMENT '电话号码',
+  `sex` char(1) DEFAULT NULL COMMENT '性别',
+  `create_time` timestamp NOT NULL COMMENT '注册时间',
+  `update_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  `status` int NOT NULL DEFAULT '1' COMMENT '账号状态',
+  `follow` int DEFAULT NULL COMMENT '关注',
+  `fans` int DEFAULT NULL COMMENT '粉丝',
+  `praised` int DEFAULT NULL COMMENT '获赞与收藏',
+  `resume` varchar(50) DEFAULT '无' COMMENT '个人简介',
+  `portrait` varchar(255) DEFAULT 'https://kelsey-webdemo.oss-cn-hangzhou.aliyuncs.com/6c07d916-8634-4d3a-b4ca-d375991b1571.jpeg' COMMENT '用户头像',
+  `place` varchar(255) DEFAULT NULL COMMENT '地区',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户信息';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (14,'YouWenXin','030530','18960935500',NULL,'2024-09-23 04:29:44',NULL,0,NULL,NULL,NULL,'无','https://kelsey-webdemo.oss-cn-hangzhou.aliyuncs.com/6c07d916-8634-4d3a-b4ca-d375991b1571.jpeg',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role` (
+  `user_id` bigint NOT NULL COMMENT '用户id',
+  `role_id` bigint NOT NULL COMMENT '角色id',
+  PRIMARY KEY (`role_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户角色关联表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (14,1);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-29 11:26:09
